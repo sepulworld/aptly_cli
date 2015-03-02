db6ae20db34b3dc2a572236c935196e0e97be951
Zane <zane.williamson@gmail.com>
2015-03-01 18:46:58 -0800

    adding new tests and refactor initialize in aptly load library

---
+++ test/test_aptly_cli_configure.rb
@@ -0,0 +1,31 @@
+require_relative 'minitest_helper'
+require 'aptly_cli'
+
+class TestAptlyCli < Minitest::Unit::TestCase 
+
+  attr_reader :test_aptly_load
+
+  def setup
+    @test_aptly_load = ::AptlyLoad.new
+  end
+
+  def test_that_it_has_a_version_number
+    refute_nil ::AptlyCli::VERSION
+  end
+
+  def test_that_config_loads
+    assert_equal ({:server => "127.0.0.2", :port => 8083}), @test_aptly_load.configure(opts = {:server => "127.0.0.2", :port => 8083 })
+  end
+  
+  def test_that_config_loads_from_yaml
+    assert_equal ({:server => "127.0.0.1", :port => 8082}), @test_aptly_load.configure_with('test/fixtures/aptly-cli.yaml')
+  end
+
+  def test_that_config_loads_defaults_if_bad_yaml
+    assert_equal ({:server => "127.0.0.1", :port => 8081}), @test_aptly_load.configure_with('test/fixtures/aptly-cli_invalid.yaml')
+  end
+  
+  def test_that_config_loads_defaults_if_no_yaml
+    assert_equal ({:server => "127.0.0.1", :port => 8081}), @test_aptly_load.configure_with('test/fixtures/aptly-cli_no_yaml.yaml')
+  end
+end
