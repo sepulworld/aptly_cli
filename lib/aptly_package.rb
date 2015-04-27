24732b7ac893bbd86791f0beab85fa4b4678e0a2
Zane <zane.williamson@gmail.com>
2015-04-27 09:08:13 -0700

    adding package show with package_key, func and fixture

---
+++ lib/aptly_package.rb
@@ -0,0 +1,21 @@
+require "aptly_cli/version"
+require "aptly_load"
+require "httmultiparty"
+require "json"
+
+module AptlyCli
+  class AptlyPackage
+
+    include HTTMultiParty
+
+    # Load aptly-cli.conf and establish base_uri
+    config = AptlyCli::AptlyLoad.new.configure_with("/etc/aptly-cli.conf")
+    base_uri "http://#{config[:server]}:#{config[:port]}/api"
+
+    def package_show(package_key)
+      uri = "/packages/#{package_key}"
+      self.class.get(uri)
+    end
+
+  end
+end
