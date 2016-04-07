03a10e932e192ce8d595d6f820d1e2a330d2f5f9
Zane <zane.williamson@gmail.com>
2016-04-06 21:23:38 -0700

    changing path to lib

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,6 +1,6 @@
+require 'coveralls'
+Coveralls.wear!
+
 $LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
 require 'aptly_cli'
-require 'coveralls'
 require 'minitest/autorun'
-
-Coveralls.wear!
