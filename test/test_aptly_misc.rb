569d793b9d6ddf017ad65db8112e6f26e7edb152
Zane <zane.williamson@gmail.com>
2016-04-11 19:23:54 -0700

    fixing tests

--- test/test_aptly_misc.rb
+++ test/test_aptly_misc.rb
@@ -8,35 +8,37 @@ describe AptlyCli::AptlyMisc do
     AptlyCli::AptlyMisc.must_include HTTMultiParty
   end
 
-describe 'Test if config contains username' do
-  let(:misc_api) { AptlyCli::AptlyMisc }
+  describe 'Test if config contains username' do
+    let(:misc_api) { AptlyCli::AptlyMisc }
 
-  def test_config_password_check
-    refute_includes ({ password: 'test' }), misc_api.instance_variable_get(:@config)
+    def test_config_username_check
+      refute_equal misc_api.instance_variable_get(:@config).to_h,
+        :server=>"192.168.64.2", :port=>8082, :debug=>false, :username=>'test'
+    end
+
+    def test_config_password_check
+      refute_equal misc_api.instance_variable_get(:@config).to_h,
+        :server=>"192.168.64.2", :port=>8082, :debug=>false, :username=>'test', :password=>'test'
+    end
   end
 
-  def test_config_username_check
-    refute_includes ({ username: 'test' }), misc_api.instance_variable_get(:@config)
+  describe 'API Get graph' do
+    let(:misc_api) { AptlyCli::AptlyMisc.new }
+
+    def test_graph_request_for_SVG_returns_200 
+      assert_equal ('200'), misc_api.get_graph(extension = 'svg').code.to_s
+    end
+
+    def test_graph_request_for_PNG_returns_200
+      assert_equal ('200'), misc_api.get_graph(extension = 'png').code.to_s
+    end
   end
-end
 
-describe 'API Get graph' do
-  let(:misc_api) { AptlyCli::AptlyMisc.new }
+  describe 'API Get Version' do
+    let(:misc_api) { AptlyCli::AptlyMisc.new }
 
-  def test_graph_request_for_SVG_returns_200 
-    assert_equal ('200'), misc_api.get_graph(extension = 'svg').code.to_s
+    def test_version_returns_valid
+      assert_equal '{"Version"=>"0.9.6"}', misc_api.get_version().to_s
+    end
   end
-
-  def test_graph_request_for_PNG_returns_200
-    assert_equal ('200'), misc_api.get_graph(extension = 'png').code.to_s
-  end
- end
-
-describe 'API Get Version' do
-  let(:misc_api) { AptlyCli::AptlyMisc.new }
-
-  def test_version_returns_valid
-    assert_equal '{"Version"=>"0.9.6"}', misc_api.get_version().to_s
-  end
- end
 end
