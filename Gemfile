e96ee1ee7cc34a127ae54f9a4a12a7e80d4d2902
Marc Abramowitz <marc@marc-abramowitz.com>
2016-07-21 16:18:38 -0700

    Enable getting simplecov reports locally
    Without this I wasn't getting a `coverage/index.html` file when running
    `rake test`.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in aptly_cli.gemspec
 gemspec
+
+group :test do
+  gem "simplecov", require: false
+end
--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,5 +1,14 @@
 require 'coveralls'
-Coveralls.wear!
+require 'simplecov'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+  add_filter '/\.bundle/'
+end
 
 $LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
 require 'aptly_cli'
